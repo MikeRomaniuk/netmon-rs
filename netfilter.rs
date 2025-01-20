23db5e86fb289d1e152bd422fdf21ddc7aed8afd
Mike <romaniuk.mikhail004@gmail.com>
2025-01-20 15:55:53 +0000

    feat: implement packet printing

--- netfilter.rs
+++ netfilter.rs
@@ -1,6 +1,9 @@
 //! Network filter abstractions.
 
-use kernel::netfilter::{nf_hook_ops, nf_hookfn};
+use core::cell::UnsafeCell;
+use kernel::netfilter::{in_addr, iphdr, nf_hook_ops, nf_hookfn, sk_buff, tcphdr, udphdr};
+// `netfilter` is my bindings crate with all headers I need.
+use kernel::netfilter;
 use kernel::prelude::*;
 
 #[pin_data]
@@ -48,61 +51,61 @@ impl NetFilterHookOps {
     }
 }
 
-#[repr(u8)]
+#[repr(u32)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum ProtocolFamily {
-    Unspec = 0,
-    Unix = 1,
-    Inet = 2,
-    Ax25 = 3,
-    Ipx = 4,
-    Appletalk = 5,
-    Netrom = 6,
-    Bridge = 7,
-    Atmpvc = 8,
-    X25 = 9,
-    Inet6 = 10,
-    Rose = 11,
-    Decnet = 12,
-    Netbeui = 13,
-    Security = 14,
-    Key = 15,
-    Netlink = 16,
-    Packet = 17,
-    Ash = 18,
-    Econet = 19,
-    Atmsvc = 20,
-    Rds = 21,
-    Sna = 22,
-    Irda = 23,
-    Pppox = 24,
-    Wanpipe = 25,
-    Llc = 26,
-    Ib = 27,
-    Mpls = 28,
-    Can = 29,
-    Tipc = 30,
-    Bluetooth = 31,
-    Iucv = 32,
-    Rxrpc = 33,
-    Isdn = 34,
-    Phonet = 35,
-    Ieee802154 = 36,
-    Caif = 37,
-    Alg = 38,
-    Nfc = 39,
-    Vsock = 40,
-    Kcm = 41,
-    Qipcrtr = 42,
-    Smc = 43,
-    Xdp = 44,
-    Mctp = 45,
-    Max = 46,
+    Unspec = netfilter::AF_UNSPEC,
+    Unix = netfilter::AF_UNIX,
+    Inet = netfilter::AF_INET,
+    Ax25 = netfilter::AF_AX25,
+    Ipx = netfilter::AF_IPX,
+    Appletalk = netfilter::AF_APPLETALK,
+    Netrom = netfilter::AF_NETROM,
+    Bridge = netfilter::AF_BRIDGE,
+    Atmpvc = netfilter::AF_ATMPVC,
+    X25 = netfilter::AF_X25,
+    Inet6 = netfilter::AF_INET6,
+    Rose = netfilter::AF_ROSE,
+    Decnet = netfilter::AF_DECnet,
+    Netbeui = netfilter::AF_NETBEUI,
+    Security = netfilter::AF_SECURITY,
+    Key = netfilter::AF_KEY,
+    Netlink = netfilter::AF_NETLINK,
+    Packet = netfilter::AF_PACKET,
+    Ash = netfilter::AF_ASH,
+    Econet = netfilter::AF_ECONET,
+    Atmsvc = netfilter::AF_ATMSVC,
+    Rds = netfilter::AF_RDS,
+    Sna = netfilter::AF_SNA,
+    Irda = netfilter::AF_IRDA,
+    Pppox = netfilter::AF_PPPOX,
+    Wanpipe = netfilter::AF_WANPIPE,
+    Llc = netfilter::AF_LLC,
+    Ib = netfilter::AF_IB,
+    Mpls = netfilter::AF_MPLS,
+    Can = netfilter::AF_CAN,
+    Tipc = netfilter::AF_TIPC,
+    Bluetooth = netfilter::AF_BLUETOOTH,
+    Iucv = netfilter::AF_IUCV,
+    Rxrpc = netfilter::AF_RXRPC,
+    Isdn = netfilter::AF_ISDN,
+    Phonet = netfilter::AF_PHONET,
+    Ieee802154 = netfilter::AF_IEEE802154,
+    Caif = netfilter::AF_CAIF,
+    Alg = netfilter::AF_ALG,
+    Nfc = netfilter::AF_NFC,
+    Vsock = netfilter::AF_VSOCK,
+    Kcm = netfilter::AF_KCM,
+    Qipcrtr = netfilter::AF_QIPCRTR,
+    Smc = netfilter::AF_SMC,
+    Xdp = netfilter::AF_XDP,
+    Mctp = netfilter::AF_MCTP,
+    Max = netfilter::AF_MAX,
 }
 
 impl ProtocolFamily {
-    pub const LOCAL: ProtocolFamily = ProtocolFamily::Unix;
-    pub const ROUTE: ProtocolFamily = ProtocolFamily::Netlink;
+    const LOCAL: ProtocolFamily = ProtocolFamily::Unix;
+    const ROUTE: ProtocolFamily = ProtocolFamily::Netlink;
 }
 
 impl From<ProtocolFamily> for u8 {
@@ -114,24 +117,24 @@ impl From<ProtocolFamily> for u8 {
 #[repr(i32)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum HookPriority {
-    First = -2147483648,
-    RawBeforeDefrag = -450,
-    ConntrackDefrag = -400,
-    Raw = -300,
-    SelinuxFirst = -225,
-    Conntrack = -200,
-    Mangle = -150,
-    NatDst = -100,
-    Filter = 0,
-    Security = 50,
-    NatSrc = 100,
-    SelinuxLast = 225,
-    ConntrackHelper = 300,
-    ConntrackConfirm = 2147483647,
+    First = netfilter::nf_ip_hook_priorities_NF_IP_PRI_FIRST,
+    RawBeforeDefrag = netfilter::nf_ip_hook_priorities_NF_IP_PRI_RAW_BEFORE_DEFRAG,
+    ConntrackDefrag = netfilter::nf_ip_hook_priorities_NF_IP_PRI_CONNTRACK_DEFRAG,
+    Raw = netfilter::nf_ip_hook_priorities_NF_IP_PRI_RAW,
+    SelinuxFirst = netfilter::nf_ip_hook_priorities_NF_IP_PRI_SELINUX_FIRST,
+    Conntrack = netfilter::nf_ip_hook_priorities_NF_IP_PRI_CONNTRACK,
+    Mangle = netfilter::nf_ip_hook_priorities_NF_IP_PRI_MANGLE,
+    NatDst = netfilter::nf_ip_hook_priorities_NF_IP_PRI_NAT_DST,
+    Filter = netfilter::nf_ip_hook_priorities_NF_IP_PRI_FILTER,
+    Security = netfilter::nf_ip_hook_priorities_NF_IP_PRI_SECURITY,
+    NatSrc = netfilter::nf_ip_hook_priorities_NF_IP_PRI_NAT_SRC,
+    SelinuxLast = netfilter::nf_ip_hook_priorities_NF_IP_PRI_SELINUX_LAST,
+    ConntrackHelper = netfilter::nf_ip_hook_priorities_NF_IP_PRI_CONNTRACK_HELPER,
+    ConntrackConfirm = netfilter::nf_ip_hook_priorities_NF_IP_PRI_CONNTRACK_CONFIRM,
 }
 
 impl HookPriority {
-    pub const LAST: HookPriority = HookPriority::ConntrackConfirm;
+    pub(crate) const LAST: HookPriority = HookPriority::ConntrackConfirm;
 }
 
 impl From<HookPriority> for i32 {
@@ -143,16 +146,16 @@ impl From<HookPriority> for i32 {
 #[repr(u32)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum HookResponse {
-    Drop = 0,
-    Accept = 1,
-    Stolen = 2,
-    Queue = 3,
-    Repeat = 4,
-    Stop = 5,
+    Drop = netfilter::NF_DROP,
+    Accept = netfilter::NF_ACCEPT,
+    Stolen = netfilter::NF_STOLEN,
+    Queue = netfilter::NF_QUEUE,
+    Repeat = netfilter::NF_REPEAT,
+    Stop = netfilter::NF_STOP,
 }
 
 impl HookResponse {
-    pub const MAX_VERDICT: u32 = 5;
+    pub(crate) const MAX_VERDICT: HookResponse = HookResponse::Stop;
 }
 
 impl From<HookResponse> for u32 {
@@ -164,12 +167,12 @@ impl From<HookResponse> for u32 {
 #[repr(u32)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum HookNum {
-    PreRouting = 0,
-    LocalIn = 1,
-    Forward = 2,
-    LocalOut = 3,
-    PostRouting = 4,
-    NumHooks = 5,
+    PreRouting = netfilter::nf_inet_hooks_NF_INET_PRE_ROUTING,
+    LocalIn = netfilter::nf_inet_hooks_NF_INET_LOCAL_IN,
+    Forward = netfilter::nf_inet_hooks_NF_INET_FORWARD,
+    LocalOut = netfilter::nf_inet_hooks_NF_INET_LOCAL_OUT,
+    PostRouting = netfilter::nf_inet_hooks_NF_INET_POST_ROUTING,
+    NumHooks = netfilter::nf_inet_hooks_NF_INET_NUMHOOKS,
 }
 
 impl HookNum {
@@ -179,5 +182,308 @@ impl HookNum {
 impl From<HookNum> for u32 {
     fn from(value: HookNum) -> Self {
         value as u32
+    }
+}
+
+#[repr(u32)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum IpProtocol {
+    Ip = netfilter::IPPROTO_IP,
+    Icmp = netfilter::IPPROTO_ICMP,
+    Igmp = netfilter::IPPROTO_IGMP,
+    Ipip = netfilter::IPPROTO_IPIP,
+    Tcp = netfilter::IPPROTO_TCP,
+    Egp = netfilter::IPPROTO_EGP,
+    Pup = netfilter::IPPROTO_PUP,
+    Udp = netfilter::IPPROTO_UDP,
+    Idp = netfilter::IPPROTO_IDP,
+    Tp = netfilter::IPPROTO_TP,
+    Dccp = netfilter::IPPROTO_DCCP,
+    Ipv6 = netfilter::IPPROTO_IPV6,
+    Rsvp = netfilter::IPPROTO_RSVP,
+    Gre = netfilter::IPPROTO_GRE,
+    Esp = netfilter::IPPROTO_ESP,
+    Ah = netfilter::IPPROTO_AH,
+    Mtp = netfilter::IPPROTO_MTP,
+    Beetph = netfilter::IPPROTO_BEETPH,
+    Encap = netfilter::IPPROTO_ENCAP,
+    Pim = netfilter::IPPROTO_PIM,
+    Comp = netfilter::IPPROTO_COMP,
+    L2tp = netfilter::IPPROTO_L2TP,
+    Sctp = netfilter::IPPROTO_SCTP,
+    Udplite = netfilter::IPPROTO_UDPLITE,
+    Mpls = netfilter::IPPROTO_MPLS,
+    Ethernet = netfilter::IPPROTO_ETHERNET,
+    Raw = netfilter::IPPROTO_RAW,
+    Mptcp = netfilter::IPPROTO_MPTCP,
+    Max = netfilter::IPPROTO_MAX,
+}
+
+impl From<IpProtocol> for u32 {
+    fn from(value: IpProtocol) -> Self {
+        value as u32
+    }
+}
+
+impl TryFrom<u32> for IpProtocol {
+    type Error = &'static str;
+
+    fn try_from(value: u32) -> Result<Self, Self::Error> {
+        match value {
+            netfilter::IPPROTO_IP => Ok(Self::Ip),
+            netfilter::IPPROTO_ICMP => Ok(Self::Icmp),
+            netfilter::IPPROTO_IGMP => Ok(Self::Igmp),
+            netfilter::IPPROTO_IPIP => Ok(Self::Ipip),
+            netfilter::IPPROTO_TCP => Ok(Self::Tcp),
+            netfilter::IPPROTO_EGP => Ok(Self::Egp),
+            netfilter::IPPROTO_PUP => Ok(Self::Pup),
+            netfilter::IPPROTO_UDP => Ok(Self::Udp),
+            netfilter::IPPROTO_IDP => Ok(Self::Idp),
+            netfilter::IPPROTO_TP => Ok(Self::Tp),
+            netfilter::IPPROTO_DCCP => Ok(Self::Dccp),
+            netfilter::IPPROTO_IPV6 => Ok(Self::Ipv6),
+            netfilter::IPPROTO_RSVP => Ok(Self::Rsvp),
+            netfilter::IPPROTO_GRE => Ok(Self::Gre),
+            netfilter::IPPROTO_ESP => Ok(Self::Esp),
+            netfilter::IPPROTO_AH => Ok(Self::Ah),
+            netfilter::IPPROTO_MTP => Ok(Self::Mtp),
+            netfilter::IPPROTO_BEETPH => Ok(Self::Beetph),
+            netfilter::IPPROTO_ENCAP => Ok(Self::Encap),
+            netfilter::IPPROTO_PIM => Ok(Self::Pim),
+            netfilter::IPPROTO_COMP => Ok(Self::Comp),
+            netfilter::IPPROTO_L2TP => Ok(Self::L2tp),
+            netfilter::IPPROTO_SCTP => Ok(Self::Sctp),
+            netfilter::IPPROTO_UDPLITE => Ok(Self::Udplite),
+            netfilter::IPPROTO_MPLS => Ok(Self::Mpls),
+            netfilter::IPPROTO_ETHERNET => Ok(Self::Ethernet),
+            netfilter::IPPROTO_RAW => Ok(Self::Raw),
+            netfilter::IPPROTO_MPTCP => Ok(Self::Mptcp),
+            netfilter::IPPROTO_MAX => Ok(Self::Max),
+            _ => Err("unknown protocol value"),
+        }
+    }
+}
+
+/// Wraps the kernel's `struct iphdr`.
+#[repr(transparent)]
+pub(crate) struct IpHeader(UnsafeCell<iphdr>);
+
+impl IpHeader {
+    pub(crate) fn protocol(&self) -> IpProtocol {
+        // SAFETY: The existence of a shared reference means `self.0` is valid.
+        u32::from(unsafe { core::ptr::addr_of!((*self.0.get()).protocol).read() })
+            .try_into()
+            .expect("could not get protocol")
+    }
+
+    pub(crate) fn source_addr(&self) -> Ipv4Addr {
+        // SAFETY: The existence of a shared reference means `self.0` is valid.
+        let s_addr =
+            unsafe { core::ptr::addr_of!((*self.0.get()).__bindgen_anon_1.addrs.saddr).read() };
+        Ipv4Addr(in_addr { s_addr })
+    }
+
+    pub(crate) fn destination_addr(&self) -> Ipv4Addr {
+        // SAFETY: The existence of a shared reference means `self.0` is valid.
+        let s_addr =
+            unsafe { core::ptr::addr_of!((*self.0.get()).__bindgen_anon_1.addrs.daddr).read() };
+        Ipv4Addr(in_addr { s_addr })
+    }
+}
+/// Wraps the kernel's `struct sk_buff`.
+///
+/// Took part of the implementaion from an [`old API`](https://rust-for-linux.github.io/docs/rust/src/kernel/net.rs.html#65-99)
+#[repr(transparent)]
+pub(crate) struct SkBuff(UnsafeCell<sk_buff>);
+
+impl SkBuff {
+    /// Creates a reference to an [`SkBuff`] from a valid pointer.
+    ///
+    /// # Safety
+    ///
+    /// The caller must ensure that `ptr` is valid and remains valid for the lifetime of the
+    /// returned [`SkBuff`] instance.
+    pub(crate) unsafe fn from_ptr<'a>(ptr: *const sk_buff) -> &'a SkBuff {
+        // SAFETY: The safety requirements guarantee the validity of the dereference, while the
+        // `SkBuff` type being transparent makes the cast ok.
+        unsafe { &*ptr.cast() }
+    }
+
+    /// Returns the total length of the data (in all segments) in the skb.
+    pub(crate) fn len(&self) -> u32 {
+        // SAFETY: The existence of a shared reference means `self.0` is valid.
+        unsafe { core::ptr::addr_of!((*self.0.get()).len).read() }
+    }
+
+    pub(crate) fn data_len(&self) -> u32 {
+        // SAFETY: The existence of a shared reference means `self.0` is valid.
+        unsafe { core::ptr::addr_of!((*self.0.get()).data_len).read() }
+    }
+
+    pub(crate) fn mac_header(&self) -> &[u8] {
+        let len = if self.is_nonlinear() {
+            self.data_len()
+        } else {
+            self.len()
+        }
+        .try_into()
+        .expect("could not convert length of mac header");
+
+        // SAFETY: The existence of a shared reference means `self.0` is valid.
+        let data = unsafe {
+            core::ptr::addr_of!((*self.0.get()).head)
+                .read()
+                .wrapping_add(
+                    core::ptr::addr_of!(
+                        (*self.0.get()).__bindgen_anon_4.headers.as_ref().mac_header
+                    )
+                    .read()
+                    .into(),
+                )
+        };
+
+        // SAFETY: The `struct sk_buff` conventions guarantee that at least `skb_mac_header_len(skb)` bytes
+        // are valid from `skb->mac_header`.
+        unsafe { core::slice::from_raw_parts(data, len) }
+    }
+
+    fn get_network_header_addr(&self) -> *mut u8 {
+        // SAFETY: The existence of a shared reference means `self.0` is valid.
+        unsafe {
+            core::ptr::addr_of!((*self.0.get()).head)
+                .read()
+                .wrapping_add(
+                    core::ptr::addr_of!(
+                        (*self.0.get())
+                            .__bindgen_anon_4
+                            .headers
+                            .as_ref()
+                            .network_header
+                    )
+                    .read()
+                    .into(),
+                )
+        }
+    }
+
+    pub(crate) fn get_ip_header(&self) -> &IpHeader {
+        let addr = self.get_network_header_addr();
+        // SAFETY: The safety requirements guarantee the validity of the dereference, while the
+        // `IpHeader` type being transparent makes the cast ok.
+        unsafe { &*addr.cast::<IpHeader>() }
+    }
+
+    pub(crate) fn is_nonlinear(&self) -> bool {
+        self.data_len() != 0
+    }
+
+    pub(crate) fn transport_header(&self) -> *mut u8 {
+        unsafe {
+            core::ptr::addr_of!((*self.0.get()).head)
+                .read()
+                .wrapping_add(
+                    core::ptr::addr_of!(
+                        (*self.0.get())
+                            .__bindgen_anon_4
+                            .headers
+                            .as_ref()
+                            .transport_header
+                    )
+                    .read()
+                    .into(),
+                )
+        }
+    }
+}
+
+/// An IPv4 address.
+///
+/// This is equivalent to C's `in_addr`.
+#[repr(transparent)]
+#[derive(Clone, Copy)]
+pub(crate) struct Ipv4Addr(in_addr);
+
+impl Ipv4Addr {
+    /// A wildcard IPv4 address.
+    ///
+    /// Binding to this address means binding to all IPv4 addresses.
+    // pub(crate) const ANY: Self = Self::new(0, 0, 0, 0);
+
+    /// The IPv4 loopback address.
+    // pub(crate) const LOOPBACK: Self = Self::new(127, 0, 0, 1);
+
+    /// The IPv4 broadcast address.
+    // pub(crate) const BROADCAST: Self = Self::new(255, 255, 255, 255);
+
+    /// Creates a new IPv4 address with the given components.
+    #[allow(dead_code)]
+    pub(crate) const fn new(a: u8, b: u8, c: u8, d: u8) -> Self {
+        Self(in_addr {
+            s_addr: u32::from_be_bytes([a, b, c, d]),
+        })
+    }
+}
+
+impl PartialEq for Ipv4Addr {
+    fn eq(&self, other: &Self) -> bool {
+        self.0.s_addr.eq(&other.0.s_addr)
+    }
+}
+
+impl core::fmt::Debug for Ipv4Addr {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        let bytes = self.0.s_addr.to_le_bytes();
+
+        write!(f, "{}.{}.{}.{}", bytes[0], bytes[1], bytes[2], bytes[3])
+    }
+}
+
+#[repr(transparent)]
+pub(crate) struct TcpHeader(UnsafeCell<tcphdr>);
+
+impl TcpHeader {
+    /// Creates a reference to an [`TcpHeader`] from a valid pointer.
+    ///
+    /// # Safety
+    ///
+    /// The caller must ensure that `ptr` is valid and remains valid for the lifetime of the
+    /// returned [`TcpHeader`] instance.
+    pub(crate) unsafe fn from_ptr<'a>(ptr: *const tcphdr) -> &'a TcpHeader {
+        // SAFETY: The safety requirements guarantee the validity of the dereference, while the
+        // `TcpHeader` type being transparent makes the cast ok.
+        unsafe { &*ptr.cast() }
+    }
+
+    pub(crate) fn destination_port(&self) -> u16 {
+        unsafe { core::ptr::addr_of!((*self.0.get()).dest).read() }.swap_bytes()
+    }
+
+    pub(crate) fn source_port(&self) -> u16 {
+        unsafe { core::ptr::addr_of!((*self.0.get()).source).read() }.swap_bytes()
+    }
+}
+
+#[repr(transparent)]
+pub(crate) struct UdpHeader(UnsafeCell<udphdr>);
+
+impl UdpHeader {
+    /// Creates a reference to an [`UdpHeader`] from a valid pointer.
+    ///
+    /// # Safety
+    ///
+    /// The caller must ensure that `ptr` is valid and remains valid for the lifetime of the
+    /// returned [`UdpHeader`] instance.
+    pub(crate) unsafe fn from_ptr<'a>(ptr: *const udphdr) -> &'a UdpHeader {
+        // SAFETY: The safety requirements guarantee the validity of the dereference, while the
+        // `UdpHeader` type being transparent makes the cast ok.
+        unsafe { &*ptr.cast() }
+    }
+
+    pub(crate) fn destination_port(&self) -> u16 {
+        unsafe { core::ptr::addr_of!((*self.0.get()).dest).read() }
+    }
+
+    pub(crate) fn source_port(&self) -> u16 {
+        unsafe { core::ptr::addr_of!((*self.0.get()).source).read() }
     }
 }
--- netmon.rs
+++ netmon.rs
@@ -4,12 +4,14 @@ mod netfilter;
 
 use core::pin::Pin;
 use kernel::error::to_result;
-use kernel::netfilter::{
-    init_net, nf_hook_state, nf_register_net_hook,
-    nf_unregister_net_hook, sk_buff,
-};
+// `netfilter` is my bindings crate with all headers I need.
+use kernel::netfilter::{nf_hook_state, nf_register_net_hook, nf_unregister_net_hook, sk_buff};
+use kernel::pr_cont;
 use kernel::prelude::*;
-use netfilter::{HookPriority, HookResponse, NetFilterHookOps, ProtocolFamily, HookNum};
+use netfilter::{
+    HookNum, HookPriority, HookResponse, IpProtocol, Ipv4Addr, NetFilterHookOps, ProtocolFamily,
+    SkBuff, TcpHeader, UdpHeader,
+};
 
 module! {
     type: NetMon,
@@ -19,8 +21,103 @@ module! {
     license: "GPL",
 }
 
+// This won't be in an async context, so there is no sense to wrap them in Muxex/whatever.
+static mut PROTOCOLS: Vec<IpProtocol> = Vec::new();
+static mut ADDRS: Vec<Ipv4Addr> = Vec::new();
+static mut PORTS: Vec<u16> = Vec::new();
+
 struct NetMon {
     nfho: Pin<Box<NetFilterHookOps>>,
+}
+
+impl NetMon {
+    fn handle_packet(skb: &SkBuff) {
+        let iph = skb.get_ip_header();
+
+        let source_addr = iph.source_addr();
+        let destination_addr = iph.destination_addr();
+
+        if unsafe {
+            !ADDRS.is_empty()
+                && (!ADDRS.contains(&source_addr) && !ADDRS.contains(&destination_addr))
+        } {
+            return;
+        }
+
+        let protocol = iph.protocol();
+
+        if unsafe { !PROTOCOLS.is_empty() && !PROTOCOLS.contains(&protocol) } {
+            return;
+        }
+
+        // TODO: abstract this with TransportLayerProtocol trait;
+        let (destination_port, source_port) = match protocol {
+            IpProtocol::Tcp => {
+                let tcp = unsafe { TcpHeader::from_ptr(skb.transport_header() as *const _) };
+                (tcp.destination_port(), tcp.source_port())
+            }
+            IpProtocol::Udp => {
+                let udp = unsafe { UdpHeader::from_ptr(skb.transport_header() as *const _) };
+                (udp.destination_port(), udp.source_port())
+            }
+            _ => {
+                pr_info!("Unsoported protocol {protocol:?}");
+                return;
+            }
+        };
+
+        if unsafe {
+            !PORTS.is_empty()
+                && (!PORTS.contains(&source_port) && !PORTS.contains(&destination_port))
+        } {
+            return;
+        }
+
+        pr_info!("{protocol:?}: {source_addr:?}:{source_port} -> {destination_addr:?}:{destination_port}\n");
+        Self::print_packet(skb);
+    }
+
+    fn print_packet(skb: &SkBuff) {
+        const ROW_SIZE: usize = 16;
+
+        pr_info!("Packet hex dump:\n");
+
+        let data = skb.mac_header();
+
+        for (line_num, chunk) in data.chunks(ROW_SIZE).enumerate() {
+            pr_info!("{:0>6}\t", line_num * 10);
+
+            for byte in chunk {
+                pr_cont!("{:02X} ", byte);
+            }
+
+            pr_cont!("\n");
+        }
+    }
+
+    fn new(mut nfho: Pin<Box<NetFilterHookOps>>) -> Result<Self, kernel::error::Error> {
+        use kernel::netfilter::init_net;
+
+        to_result(unsafe {
+            nf_register_net_hook(
+                &mut init_net as *mut _,
+                &nfho.as_mut().get_unchecked_mut().inner as *const _,
+            )
+        })?;
+
+        Ok(Self { nfho })
+    }
+
+    fn unregister_net_hook(&mut self) {
+        use kernel::netfilter::init_net;
+
+        unsafe {
+            nf_unregister_net_hook(
+                &mut init_net as *mut _,
+                &self.nfho.as_mut().get_unchecked_mut().inner as *const _,
+            );
+        }
+    }
 }
 
 unsafe impl Send for NetMon {}
@@ -38,39 +135,35 @@ impl kernel::Module for NetMon {
             nfho.set_priority(HookPriority::First)
         }
 
-        to_result(unsafe {
-            nf_register_net_hook(
-                &mut init_net as *mut _,
-                &nfho.as_mut().get_unchecked_mut().inner as *const _,
-            )
-        })?;
+        let netmon = NetMon::new(nfho)?;
+
+        unsafe {
+            PORTS.try_push(443)?;
+            PROTOCOLS.try_push(IpProtocol::Tcp)?;
+            PROTOCOLS.try_push(IpProtocol::Udp)?;
+        };
 
         pr_info!("Rust Network Monitor (init)\n");
-        Ok(NetMon { nfho })
+        Ok(netmon)
     }
 }
 
 impl Drop for NetMon {
     fn drop(&mut self) {
-        unsafe {
-            nf_unregister_net_hook(
-                &mut init_net as *mut _,
-                &self.nfho.as_mut().get_unchecked_mut().inner as *const _,
-            );
-        }
+        self.unregister_net_hook();
         pr_info!("Rust Network Monitor (exit)\n");
     }
 }
 
-/// Netfilter hook function. 
-/// 
-/// This function will be registered in the kernel 
+/// Netfilter hook function.
+///
+/// This function will be registered in the kernel
 /// to be called at specific points in the network stack.
-/// 
+///
 /// Function registration is done by inserting `nf_hook_ops` with the [`nf_register_hook`] function.
-/// 
+///
 /// # Safety
-/// 
+///
 /// This function is safe, since the validity of the `skb` is checked.
 pub unsafe extern "C" fn hook_fn(
     _priv_: *mut core::ffi::c_void,
@@ -78,14 +171,16 @@ pub unsafe extern "C" fn hook_fn(
     _state: *const nf_hook_state,
 ) -> core::ffi::c_uint {
     // SAFETY: if `skb` is a null-pointer, the [`Option::None`] is returned by the `as_ref()` function.
-    let skb_option = unsafe { skb.as_ref() };
-    pr_info!("I am in this thing!");
-    match skb_option {
-        Some(_skb) => {
-            HookResponse::Accept.into()
+    match unsafe { skb.as_ref() } {
+        Some(_) => {
+            // SAFETY: if `skb` was a null-pointer, we would never be in `Some` branch.
+            let skb = unsafe { SkBuff::from_ptr(skb) };
+            NetMon::handle_packet(skb);
         }
         None => {
-            HookResponse::Accept.into()
+            pr_err!("skb is None");
         }
     }
+
+    HookResponse::Accept.into()
 }
